96002ddece1b83fa5eb3b3e3b56b476f461ea0c2
kiki-kanri <a470666@gmail.com>
2025-04-19 05:24:30 +0800

    test: update jest and tsconfig.jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 /** @type {import('jest').Config} */
 module.exports = {
+    clearMocks: true,
     testEnvironment: 'node',
     transform: {
         '^.+\\.tsx?$': [
             'ts-jest',
-            { tsconfig: 'tsconfig.jest.json' },
+            { tsconfig: './tsconfig.jest.json' },
         ],
     },
+    workerThreads: true,
 };
